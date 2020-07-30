9415458d00702f932b5ab3a0cd25f53103e2b83b
Giuliano Dore <giuliano@gogetgroomed.co.uk>
2020-07-30 15:07:26 +0100

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
 var path = require('path');
 
 module.exports = {
-  devtool: 'source-map',
-  entry: './index.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, './')
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015','react']
-        }
-      },
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader',
-        exclude: /node_modules/
-      }
-    ]
-  }
+	watch: true,
+	devtool: 'source-map',
+	entry: './index.jsx',
+	output: {
+		filename: 'bundle.js',
+		path: path.join(__dirname, './')
+	},
+	module: {
+		rules: [
+		  {
+			test: /\.jsx$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+			}
+		  },
+		  {
+			test: /\.css$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'style-loader!css-loader',
+			}
+		  }
+		]
+	}
 };
